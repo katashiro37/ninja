041c83a4ac7a2f880855953cbc797f63025ef44b
mikee.mikee <mikee.mikee@onehundred.ph>
2018-01-09 16:30:39 +0800

    added gravatar to contacts table row

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
   belongs_to :group
+
+  def gravatar
+      # create the md5 hash
+    hash = Digest::MD5.hexdigest(email.downcase)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    "https://www.gravatar.com/avatar/#{hash}"
+  end
 end
