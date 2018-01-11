1fa40bfb0999ab65b1ccf26493ccf246c7872cd3
mikee.mikee <mikee.mikee@onehundred.ph>
2018-01-11 16:06:35 +0800

    add contact now functional, storing to databse

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,31 @@
 class ContactsController < ApplicationController
-  def index
-  	#@contacts = Contact.all
-  	#@contacts = Contact.page.per(10) kaminari GEM
-    if params[:group_id] && !params[:group_id].empty?
-       #@contacts = Contact.where(group_id: params[:group_id]).page(params[:page]) #use where method for group_id param
-       @contacts = Group.find(params[:group_id]).contacts.page(params[:page]) # another way to get group_id
-    else
-  	   @contacts = Contact.page(params[:page]) #with default page set on kaminari Config
+    def index
+    	#@contacts = Contact.all
+    	#@contacts = Contact.page.per(10) kaminari GEM
+      if params[:group_id] && !params[:group_id].empty?
+         #@contacts = Contact.where(group_id: params[:group_id]).page(params[:page]) #use where method for group_id param
+         @contacts = Group.find(params[:group_id]).contacts.page(params[:page]) # another way to get group_id
+      else
+    	   @contacts = Contact.page(params[:page]) #with default page set on kaminari Config
+      end
     end
-  end
+
+    def new
+    end
+
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+          flash[:success] = "Contact was successfully created."
+          redirect_to contacts_path
+        else
+          render 'new'
+        end
+    end
+
+    private
+
+    def contact_params
+      params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   get 'contacts/index', as: 'contacts'
+  get 'contacts/new', as: 'new_contact'
+  post '/contacts/create'
   root 'contacts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
