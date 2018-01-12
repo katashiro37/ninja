6ab645eb220da36d743ad536eca536859fbb3e03
mikee.mikee <mikee.mikee@onehundred.ph>
2018-01-12 14:01:33 +0800

    added validation to contact model, added order by created in controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,14 @@ class ContactsController < ApplicationController
     	#@contacts = Contact.page.per(10) kaminari GEM
       if params[:group_id] && !params[:group_id].empty?
          #@contacts = Contact.where(group_id: params[:group_id]).page(params[:page]) #use where method for group_id param
-         @contacts = Group.find(params[:group_id]).contacts.page(params[:page]) # another way to get group_id
+         @contacts = Group.find(params[:group_id]).order(created_at: :desc).contacts.page(params[:page]) # another way to get group_id
       else
-    	   @contacts = Contact.page(params[:page]) #with default page set on kaminari Config
+    	   @contacts = Contact.order(created_at: :desc).page(params[:page]) #with default page set on kaminari Config
       end
     end
 
     def new
+      @contact = Contact.new
     end
 
     def create
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,9 @@ class Contact < ApplicationRecord
 
   paginates_per 5
 
+  validates :name, :email, :group_id, presence: true
+  validates :name, length: {minimum: 2}
+
   def gravatar
       # create the md5 hash
     hash = Digest::MD5.hexdigest(email.downcase)
