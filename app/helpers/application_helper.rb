2e0ecaf7bde16307c7a172af31496d057a8bda47
mikee.mikee <mikee.mikee@onehundred.ph>
2018-01-22 15:34:35 +0800

    customize validation error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
       url_segments = request.fullpath.split(/\/|\?/)
       url_segments[1] === url ? 'active' : ''
     end
+
+    def has_errors?(resource, field) #dynamic error validator
+      #@contact.errors.messages[field].present?
+      resource.errors.messages[field].present?
+    end
+
+    def get_error(resource, field) #dynamic error validator
+      resource.errors.messages[field].join(', ')
+    end
+
 end
